c7fc57aef72002a0559e1353198da37e054f2bde
MichealChan <2417577316@qq.com>
2024-10-24 15:28:54 -0400

    WalkMesh

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -5,35 +5,37 @@
 
 #include <glm/glm.hpp>
 
-#include <vector>
 #include <deque>
+#include <vector>
 
 struct PlayMode : Mode {
-	PlayMode();
-	virtual ~PlayMode();
+  PlayMode();
+  virtual ~PlayMode();
 
-	//functions called by main loop:
-	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
-	virtual void update(float elapsed) override;
-	virtual void draw(glm::uvec2 const &drawable_size) override;
+  // functions called by main loop:
+  virtual bool handle_event(SDL_Event const &,
+                            glm::uvec2 const &window_size) override;
+  virtual void update(float elapsed) override;
+  virtual void draw(glm::uvec2 const &drawable_size) override;
 
-	//----- game state -----
+  //----- game state -----
 
-	//input tracking:
-	struct Button {
-		uint8_t downs = 0;
-		uint8_t pressed = 0;
-	} left, right, down, up;
+  // input tracking:
+  struct Button {
+    uint8_t downs = 0;
+    uint8_t pressed = 0;
+  } left, right, down, up;
 
-	//local copy of the game scene (so code can change it during gameplay):
-	Scene scene;
+  // local copy of the game scene (so code can change it during gameplay):
+  Scene scene;
 
-	//player info:
-	struct Player {
-		WalkPoint at;
-		//transform is at player's feet and will be yawed by mouse left/right motion:
-		Scene::Transform *transform = nullptr;
-		//camera is at player's head and will be pitched by mouse up/down motion:
-		Scene::Camera *camera = nullptr;
-	} player;
+  // player info:
+  struct Player {
+    WalkPoint at;
+    // transform is at player's feet and will be yawed by mouse left/right
+    // motion:
+    Scene::Transform *transform = nullptr;
+    // camera is at player's head and will be pitched by mouse up/down motion:
+    Scene::Camera *camera = nullptr;
+  } player;
 };
