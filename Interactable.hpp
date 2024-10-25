84216fe10f66218d0e31608f346624d56d07f406
Puca\Sasa <isa2330420@gmail.com>
2024-10-25 16:09:12 -0400

    added the interactStatus variable to furniture and item

--- Interactable.hpp
+++ Interactable.hpp
@@ -6,69 +6,74 @@
 #include <string>
 #include <vector>
 
-
 /* interactable object that can be picked up */
-enum ItemType {
+enum ItemType
+{
 	BEDROOM_KEY
 };
 
 /* Type of interactable object that cannot be picked up */
-enum FurnitureType {
+enum FurnitureType
+{
 	BED,
 	CLOSET,
 	BEDROOM_DOOR,
-	FRONT_DOOR
+	FRONT_DOOR,
+	DESK
 };
 
 /* Manages the inventory, all items, all furniture*/
-struct InteractableManager {
+struct InteractableManager
+{
 
 	Inventory inventory = Inventory();
-	std::vector<Item*> items; // better name? This collides with inventory->items
-	std::vector<Furniture*> furnitures;
-
+	std::vector<Item *> items; // better name? This collides with inventory->items
+	std::vector<Furniture *> furnitures;
 };
 
 /* Player inventory */
-struct Inventory {
+struct Inventory
+{
 
 	Inventory();
 
 	std::vector<ItemType> items; // all items
-	int size = 5; // can initially hold 5 items 
-	int current_item = 0; // the item the player currently holds. 0 = nothing 
+	int size = 5;				 // can initially hold 5 items
+	int current_item = 0;		 // the item the player currently holds. 0 = nothing
 
 	virtual bool hasItem() override;
 	virtual bool addItem() override;
 	virtual bool removeItem() override;
-
 };
 
 /* A single item */
-struct Item {
+struct Item
+{
 
 	Item();
 
 	ItemType type;
-	Scene::Transform* transform;
+	Scene::Transform *transform;
 
 	bool visible;
 	bool canInteract;
+	bool interactStatus = falses; // is the player currently interacting with it?
 
 	virtual bool interact() override;
-
 };
 
 /* A single furniture */
-struct Furniture {
+struct Furniture
+{
 
 	Furniture();
 
 	FurnitureType type;
-	Scene::Transform* transform;
+	Scene::Transform *transform;
 
 	bool visible;
 	bool canInteract;
+	bool interactStatus = false; // is the player currently interacting with it?
 
 	virtual bool interact() override;
 };
